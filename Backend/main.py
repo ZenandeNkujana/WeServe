3957c19a41071b9f84995927f8051ca825dba358
Zenandi Nkujana <zenandinkujana@gmail.com>
2026-09-11 16:25:51 +0200

    Created the user variables to save

---
+++ Backend/main.py
@@ -0,0 +1,14 @@
+# =================================
+# Save user's information
+# =================================
+#Saving the user's information so we can know who we are dealing with
+first_name = input("Enter your name: ").strip().lower()
+last_name = input("Enter your surname: ").strip().lower()
+email = input("Enter your email adress: ").strip().lower()
+password = input("Create a password containing at least 8 characters, including uppercase and lowercase letters, a number, and a special character (e.g. @, #, $): ").strip()
+address =  input("Please enter ypur address: ").strip().lower()
+gender = input("Enter your gender(Male or Female): ").strip().lower()
+
+
+
+
