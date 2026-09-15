30160e9c3c3e86459dd3a2b09fc0e882ff1330de
Zenandi Nkujana <zenandinkujana@gmail.com>
2026-09-15 15:46:00 +0200

    Done with user va;idation etc

--- Backend/main.py
+++ Backend/main.py
@@ -36,5 +36,61 @@ else:
     print(email)
     
 #password
-if len(password) < 8:
-    print("Enter a least 8 characters")
+def validate_password(password):
+
+    # Keeping track of the required 
+    has_lowercase = False
+    has_uppercase = False
+    has_number = False
+    has_special = False
+
+    # Check each character in the password
+    for character in password:
+
+        if character.islower():
+            has_lowercase = True
+
+        elif character.isupper():
+            has_uppercase = True
+
+        elif character.isdigit():
+            has_number = True
+
+        else:
+            has_special = True
+
+    # Validate the password
+    if len(password) < 8:
+        print("Enter at least 8 characters")
+
+    elif not has_lowercase:
+        print("Password must contain a lowercase letter")
+
+    elif not has_uppercase:
+        print("Password must contain an uppercase letter")
+
+    elif not has_number:
+        print("Password must contain a number")
+
+    elif not has_special:
+        print("Password must contain a special character")
+
+    else:
+        print("Password is valid")
+
+
+validate_password(password)
+
+#adress
+if address == "":
+    print("Please enter your address")
+else:
+    print(address)
+    
+#gender
+if gender == "":
+    print("Please enter your gender")
+elif gender != "male" and gender != "female":
+    print("Please enter Male or Female")
+else:
+    print(gender)
