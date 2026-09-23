7492e27d913cc07e0c77083720058129fc3f8018
Zenandi Nkujana <zenandinkujana@gmail.com>
2026-09-24 01:21:24 +0200

    Created post a job for the customer

--- Backend/Database/database.py
+++ Backend/Database/database.py
@@ -6,5 +6,20 @@ import sqlite3#I need python's built in SQLite tools.
 
 connection = sqlite3.connect("weserve.db")#Connecting my python program to the database
 
-# Create a cursor to execute SQL commands
+# Create a cursor to execute SQL commands on the db
 cursor = connection.cursor()
+
+# ========================================
+# Create the users table
+# ========================================
+
+cursor.execute("""
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY,
+        first_name TEXT,
+        second_name TEXT,
+        email TEXT,
+        password TEXT,
+        gender TEXT
+    )
+""")
