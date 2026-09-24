88102a060ebdf50bd447cf7bf342465d753dcf62
Zenandi Nkujana <zenandinkujana@gmail.com>
2026-09-24 17:01:41 +0200

    Created some validation~

--- Backend/main.py
+++ Backend/main.py
@@ -2,95 +2,147 @@
 # Save user's information
 # =================================
 #Saving the user's information so we can know who we are dealing with
-name = input("Enter your name: ").strip().lower()
-surname = input("Enter your surname: ").strip().lower()
-email = input("Enter your email adress: ").strip().lower()
-password = input("Create a password containing at least 8 characters, including uppercase and lowercase letters, a number, and a special character (e.g. @, #, $): ").strip()
-address =  input("Please enter ypur address: ").strip().lower()
-gender = input("Enter your gender(Male or Female): ").strip().lower()
+# name = input("Enter your name: ").strip().lower()
+# surname = input("Enter your surname: ").strip().lower()
+# email = input("Enter your email adress: ").strip().lower()
+# password = input("Create a password containing at least 8 characters, including uppercase and lowercase letters, a number, and a special character (e.g. @, #, $): ").strip()
+# address =  input("Please enter ypur address: ").strip().lower()
+# gender = input("Enter your gender(Male or Female): ").strip().lower()
 
-# Validate user's information
-#name
-if name == "":
-    print("Enter your name: ")
-elif not name.isalpha():
-    print("Enter a valid name")
-else:
-    print(name)
+# # Validate user's information
+# #name
+# def user_name(name):
+#     if name == "":
+#         print("Enter your name: ")
+#     elif not name.isalpha():
+#         print("Enter a valid name")
+#     else:
+#         print(name)
+# user_name(name)       
 
-#surname
-if surname == "":
-    print("Enter ayour name: ")#not final
-elif not surname.isalpha():
-    print("Please Enter a valid name")
-else:
-    print(surname)
+# #surname
+# def last_name(surname):
+#     if surname == "":
+#         print("Enter your surname: ")#not final
+#     elif not surname.isalpha():
+#         print("Please enter a valid surname")
+#     else:
+#         print(surname)
+        
+# last_name(surname)
 
-
-#email
-if email == "":
-    print("Enter your email address")
-elif "@" not in email:
-    print("Enter a valid email address")#not final
-else:
-    print(email)
+# #email
+# def user_mail(email):
+#     if email == "":
+#         print("Enter your email address")
+#     elif "@" not in email:
+#         print("Enter a valid email address")#not final
+#     else:
+#         print(email)
+# user_mail(email)
     
-#password
-def validate_password(password):
+# #password
+# def validate_password(password):
 
-    # Keeping track of the required 
-    has_lowercase = False
-    has_uppercase = False
-    has_number = False
-    has_special = False
+#     # Keeping track of the required 
+#     has_lowercase = False
+#     has_uppercase = False
+#     has_number = False
+#     has_special = False
 
-    # Check each character in the password
-    for character in password:
+#     # Check each character in the password
+#     for character in password:
 
-        if character.islower():
-            has_lowercase = True
+#         if character.islower():
+#             has_lowercase = True
 
-        elif character.isupper():
-            has_uppercase = True
+#         elif character.isupper():
+#             has_uppercase = True
 
-        elif character.isdigit():
-            has_number = True
+#         elif character.isdigit():
+#             has_number = True
 
-        else:
-            has_special = True
+#         else:
+#             has_special = True
 
-    # Validate the password
-    if len(password) < 8:
-        print("Enter at least 8 characters")
+#     # Validate the password
+#     if len(password) < 8:
+#         print("Enter at least 8 characters")
 
-    elif not has_lowercase:
-        print("Password must contain a lowercase letter")
+#     elif not has_lowercase:
+#         print("Password must contain a lowercase letter")
 
-    elif not has_uppercase:
-        print("Password must contain an uppercase letter")
+#     elif not has_uppercase:
+#         print("Password must contain an uppercase letter")
 
-    elif not has_number:
-        print("Password must contain a number")
+#     elif not has_number:
+#         print("Password must contain a number")
 
-    elif not has_special:
-        print("Password must contain a special character")
+#     elif not has_special:
+#         print("Password must contain a special character")
 
+#     else:
+#         print("Password is valid")
+
+
+# validate_password(password)
+
+# #adress
+# if address == "":
+#     print("Please enter your address")
+# else:
+#     print(address)
+    
+# #gender
+# if gender == "":
+#     print("Please enter your gender")
+# elif gender != "male" and gender != "female":
+#     print("Please enter Male or Female")
+# else:
+#     print(gender)
+    
+    
+#========================================
+#Post a job
+#========================================
+#For someone looking for services/assistance
+print("Post a Job")
+
+service = input("What service do you need? ").strip().lower()
+description = input("Describe the job: ").strip().lower()#must contain at least 100 wods
+location = input("Where is the job located? ").strip().lower
+date = input("When is it needed? ").strip()
+time = input("What time? ").strip()
+budget = input("How nuch are you willing to pay for this job? ").strip()
+
+#========================================
+# Validate a Job Post
+#========================================
+
+# Validating the service
+def validate_service(service):
+    if service == "":
+        print("Please enter the service you need")
     else:
-        print("Password is valid")
+        print(service)
 
+validate_service(service)
 
-validate_password(password)
+#Validate the description
+def validate_description(description):
+    if description == "":
+        print("Enter the description")
+    elif not description.isalpha():
+        print("Enter a valid description")
+    else:
+        print(description)
+validate_description(description)
 
-#adress
-if address == "":
-    print("Please enter your address")
-else:
-    print(address)
+#Validate the location
+def validate_kocation(location):
+    if location == "":
+        print("Enter your location!")
+    else:
+        print(location)
+        
     
-#gender
-if gender == "":
-    print("Please enter your gender")
-elif gender != "male" and gender != "female":
-    print("Please enter Male or Female")
-else:
-    print(gender)
